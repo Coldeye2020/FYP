aec49c50a01dfc8255005aa1d45d73da52ee5989
Coldeye <948283368@qq.com>
2022-04-24 10:26:37 +0800

    new

--- src/run_experiment.sh
+++ src/run_experiment.sh
@@ -49,48 +49,48 @@
 
 
 id=0
-trian_ratio=0.6
-val_ratio=0.2
+trian_ratio=0.025
+val_ratio=0.025
 
-for dataset in cora citeseer pubmed computers photo
-do
-    for passing_type in Attention # Origin
-    do
-        for lr in 0.002 0.01 0.02 0.05
-        do 
-            for weight_decay in 0 0.0005
-            do
-                for hidden in 64 128
-                do
-                    for dprate in 0.3 0.5 0.7
-                    do
-                        for alpha in 0.1 0.2 0.5 0.9 1.0
-                        do
-                            echo "part1: $id"
-                            python model.py --model GPRGNN \
-                                --dataset $dataset \
-                                --lr $lr \
-                                --passing_type $passing_type \
-                                --weight_decay  $weight_decay \
-                                --hidden $hidden \
-                                --epoch 500 \
-                                --dprate $dprate \
-                                --alpha $alpha \
-                                --id $id
-                                --trian_ratio $trian_ratio
-                                --val_ratio $val_ratio
-                            let id++
-                        done
-                    done
-                done
-            done
-        done
-    done
-done
+# for dataset in cora citeseer pubmed computers photo
+# do
+#     for passing_type in Attention # Origin
+#     do
+#         for lr in 0.002 0.01 0.02 0.05
+#         do 
+#             for weight_decay in 0 0.0005
+#             do
+#                 for hidden in 64 128
+#                 do
+#                     for dprate in 0.3 0.5 0.7
+#                     do
+#                         for alpha in 0.1 0.2 0.5 0.9 1.0
+#                         do
+#                             echo "part1: $id"
+#                             python model.py --model GPRGNN \
+#                                 --dataset $dataset \
+#                                 --lr $lr \
+#                                 --passing_type $passing_type \
+#                                 --weight_decay  $weight_decay \
+#                                 --hidden $hidden \
+#                                 --epoch 500 \
+#                                 --dprate $dprate \
+#                                 --alpha $alpha \
+#                                 --id $id
+#                                 --trian_ratio $trian_ratio
+#                                 --val_ratio $val_ratio
+#                             let id++
+#                         done
+#                     done
+#                 done
+#             done
+#         done
+#     done
+# done
 
-id=1200
+# id=1200
 
-for dataset in squirrel chameleon film texas cornell
+for dataset in cora citeseer pubmed computers photo squirrel chameleon film texas cornell
 do
     for model in GPRGNN APPNP GAT GCN MLP JKNet SGC GCN-Cheby SAGE
     do
@@ -98,31 +98,28 @@ do
         then
             for passing_type in Attention Origin
             do
-                for lr in 0.002 0.01 0.02 0.05
+                for lr in 0.002 0.01 0.05
                 do 
-                    for weight_decay in 0 0.0005
+                    for hidden in 64 128
                     do
-                        for hidden in 64 128
+                        for dprate in 0.5 0.7
                         do
-                            for dprate in 0.3 0.5 0.7
+                            for alpha in 0.1 0.5 0.9
                             do
-                                for alpha in 0.1 0.2 0.5 0.9 1.0
-                                do
-                                    echo "part2: $id"
-                                    python model.py --model GPRGNN \
-                                        --dataset $dataset \
-                                        --lr $lr \
-                                        --passing_type $passing_type \
-                                        --weight_decay  $weight_decay \
-                                        --hidden $hidden \
-                                        --epoch 500 \
-                                        --dprate $dprate \
-                                        --alpha $alpha \
-                                        --id $id
-                                        --trian_ratio $trian_ratio
-                                        --val_ratio $val_ratio
-                                    let id++
-                                done
+                                echo "part1: $id"
+                                python model.py --model GPRGNN \
+                                    --dataset $dataset \
+                                    --lr $lr \
+                                    --passing_type $passing_type \
+                                    --weight_decay  0.0005 \
+                                    --hidden $hidden \
+                                    --epoch 10 \
+                                    --dprate $dprate \
+                                    --alpha $alpha \
+                                    --id $id \
+                                    --train_ratio $trian_ratio \
+                                    --val_ratio $val_ratio
+                                id=`expr $id + 1`
                             done
                         done
                     done
@@ -140,11 +137,11 @@ do
                         --lr $lr \
                         --weight_decay  0.0005 \
                         --hidden $hidden \
-                        --epoch 500 \
-                        --id $id
-                        --trian_ratio $trian_ratio
+                        --epoch 10 \
+                        --id $id \
+                        --train_ratio $trian_ratio \
                         --val_ratio $val_ratio
-                    let id++
+                    id=`expr $id + 1`
                 done
             done
         fi
